b93d4f0bfc18c74841370cfd71f19fe6eafcf78d
ArtemFilkov <f_art-87@mail.ru>
2024-09-27 22:51:37 +0700

    Add files via upload

---
+++ moaule_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 'string',1.7
+print(immutable_var)
+#immutable_var[0]=2
+#print(immutable_var) # обьект кортеж не поддерживает замену элементов
+mutable_list = [1.55,'name',155]
+print(mutable_list)
+mutable_list[1]=15.5
+print(mutable_list)
